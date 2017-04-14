6bd2a87142c229007db9411b30b4007c25da06cd
Sagar Thakur <sagregas@users.noreply.github.com>
2017-04-14 16:19:52 -0400

    Update Food+CoreDataProperties.swift

--- Food+CoreDataProperties.swift
+++ Food+CoreDataProperties.swift
@@ -2,8 +2,6 @@
 //  Food+CoreDataProperties.swift
 //  7a_EOE
 //
-//  Created by Rohith Raju on 11/27/16.
-//  Copyright © 2016 Rohith Raju. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
 //  to delete and recreate this implementation file for your updated model.
